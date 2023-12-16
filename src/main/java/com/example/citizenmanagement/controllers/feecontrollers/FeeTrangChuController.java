111b1d0e7360765447fee368eda4d309139f2f98
Tu Van An <an106203h@gmail.com>
2023-12-16 16:11:24 +0700

    hyyttt

--- src/main/java/com/example/citizenmanagement/controllers/feecontrollers/FeeTrangChuController.java
+++ src/main/java/com/example/citizenmanagement/controllers/feecontrollers/FeeTrangChuController.java
@@ -1,4 +1,21 @@
 package com.example.citizenmanagement.controllers.feecontrollers;
 
-public class FeeTrangChuController {
+import com.example.citizenmanagement.models.Model;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FeeTrangChuController implements Initializable {
+    private Circle profile_thuphi;
+    private Label name_thuphi;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
